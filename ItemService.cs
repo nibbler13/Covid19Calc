bd75c80ce0c49fd83f595b39a82230e6094c206c
nibbler13 einsamkeit <nibble@yandex.ru>
2020-04-08 10:36:01 +0300

    Initial commit

---
+++ ItemService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Calc {
+	public class ItemService {
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public int Cost { get; set; }
+		public int Count { get; set; }
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Covid19Calc {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "") {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+		public List<string> Logistics { get; set; } = new List<string> { "В пределах МКАД", "В пределах 10 км от МКАД", "В пределах 30 км от МКАД" };
+		public List<int> PatientsCount { get; set; } = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		private string costTotal;
+		public string CostTotal { 
+			get { return costTotal; }
+			set {
+				if (value != costTotal) {
+					costTotal = value;
+					NotifyPropertyChanged();
+				}
+			}
+		}
+
+
+		private string selectedLogistics;
+		public string SelectedLogistics {
+			get { return selectedLogistics; }
+			set {
+				if (value != selectedLogistics) {
+					selectedLogistics = value;
+					NotifyPropertyChanged();
+					UpdateServices();
+				}
+			}
+		}
+
+		private int selectedAdultCount;
+		public int SelectedAdultCount {
+			get { return selectedAdultCount; }
+			set {
+				if (value != selectedAdultCount) {
+					selectedAdultCount = value;
+					NotifyPropertyChanged();
+					UpdateServices();
+				}
+			}
+		}
+
+		private int selectedKidsCount;
+		public int SelectedKidsCount {
+			get { return selectedKidsCount; }
+			set {
+				if (value != selectedKidsCount) {
+					selectedKidsCount = value;
+					NotifyPropertyChanged();
+					UpdateServices();
+				}
+			}
+		}
+
+		private Brush costTotalBackground = new SolidColorBrush(Colors.Transparent);
+		public Brush CostTotalBackground {
+			get { return costTotalBackground; }
+			set {
+				if (value != costTotalBackground) {
+					costTotalBackground = value;
+					NotifyPropertyChanged();
+				}
+			}
+		}
+
+		public ObservableCollection<ItemService> Services { get; set; } = new ObservableCollection<ItemService>();
+
+		public MainWindow() {
+			InitializeComponent();
+			DataContext = this;
+		}
+
+
+		private void UpdateServices() {
+			bool isError = false;
+			if (string.IsNullOrEmpty(SelectedLogistics)) {
+				CostTotal = "не выбрано местонахождение";
+				isError = true;
+			}
+
+			if (SelectedAdultCount + SelectedKidsCount == 0) {
+				CostTotal = "не указано кол-во пациентов";
+				isError = true;
+			}
+
+			if (isError) {
+				CostTotalBackground = new SolidColorBrush(Colors.Yellow);
+				return;
+			}
+
+			Services.Clear();
+
+			if (SelectedAdultCount > 0) {
+				switch (SelectedLogistics) {
+					case "В пределах МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "1002285",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах МКАД",
+								Cost = 4650,
+								Count = 1
+							});
+						break;
+
+					case "В пределах 10 км от МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "1002286",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах 10 км от МКАД",
+								Cost = 6150,
+								Count = 1
+							});
+						break;
+
+					case "В пределах 30 км от МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "1002287",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах 30 км от МКАД",
+								Cost = 8150,
+								Count = 1
+							});
+						break;
+
+					default:
+						break;
+				}
+
+				if (SelectedAdultCount > 1)
+					Services.Add(
+						new ItemService {
+							Id = "101899",
+							Name = "Взятие биоматериала на ПЦР исследование на коронавирус",
+							Cost = 650,
+							Count = SelectedAdultCount - 1
+						});
+
+				if (SelectedKidsCount > 0)
+					Services.Add(
+						new ItemService {
+							Id = "212066",
+							Name = "Взятие биоматериала на ПЦР исследование на коронавирус",
+							Cost = 650,
+							Count = SelectedKidsCount
+						});
+
+			} else if (SelectedKidsCount > 0) {
+				switch (SelectedLogistics) {
+					case "В пределах МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "2110635",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах МКАД",
+								Cost = 4650,
+								Count = 1
+							});
+						break;
+					case "В пределах 10 км от МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "2110636",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах 10 км от МКАД",
+								Cost = 6150,
+								Count = 1
+							});
+						break;
+					case "В пределах 30 км от МКАД":
+						Services.Add(
+							new ItemService {
+								Id = "2110637",
+								Name = "Взятие биоматериала на ПЦР исследование на коронавирус на дому в пределах 30 км от МКАД",
+								Cost = 8150,
+								Count = 1
+							});
+						break;
+					default:
+						break;
+				}
+
+				if (SelectedKidsCount > 1)
+					Services.Add(
+						new ItemService {
+							Id = "212066",
+							Name = "Взятие биоматериала на ПЦР исследование на коронавирус",
+							Cost = 650,
+							Count = SelectedKidsCount - 1
+						});
+			}
+
+			Services.Add(
+				new ItemService { 
+					Id = "326217",
+					Name = "Определение РНК возбудителя коронавирусной инфекции COVID-19 у лиц, " +
+						"не имеющих признаков инфекционного заболевания и не находящихся в прямом " + 
+						"контакте с больным новой коронавирусной инфекцией, методом ПЦР", 
+					Cost = 1350, 
+					Count = SelectedAdultCount + SelectedKidsCount });
+
+			int cost = 0;
+			foreach (ItemService item in Services)
+				cost += item.Cost * item.Count;
+
+			CostTotalBackground = new SolidColorBrush(Colors.LightGreen);
+			CostTotal = cost.ToString("N0", CultureInfo.CurrentCulture) + " руб.";
+		}
+	}
+}
